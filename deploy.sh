0172b9790741955b1a2dad66452c23494b0c2d6a
chenxiaolani <2524729719@qq.com>
2020-10-17 14:16:09 +0800

    blog backup

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,11 @@ hugo
 # 备份博客内容
 git add .
 git commit -m "blog backup"
-git push origin git@github.com:mywebc/blogBackups.git
+git push origin git@github.com:mywebc/blogBackups.git master
 # 推送静态文件到githubPage
 cd public
 git add .
 git commit -m "blog update"
-git push origin git@github.com:mywebc/mywebc.github.io.git
+git push origin git@github.com:mywebc/mywebc.github.io.git master
 # 退出
 cd ../
