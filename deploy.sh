ef14271e80ba44d3e3f1715565d2cb80cfa158f6
chenxiaolani <2524729719@qq.com>
2020-10-17 14:15:01 +0800

    blog backup

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-# source deploy.sh 一键部署和备份博客
+# source ./deploy.sh 一键部署和备份博客
 # 删除public
 rm -rf public/
 # 生成public
@@ -6,11 +6,11 @@ hugo
 # 备份博客内容
 git add .
 git commit -m "blog backup"
-git push
+git push origin git@github.com:mywebc/blogBackups.git
 # 推送静态文件到githubPage
-cd ./public
+cd public
 git add .
 git commit -m "blog update"
-git push
+git push origin git@github.com:mywebc/mywebc.github.io.git
 # 退出
 cd ../
