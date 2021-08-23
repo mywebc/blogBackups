d6e6f1c9fa27aa637b4468865e6cb8909273f759
chenxiaolani <2524729719@qq.com>
2021-08-23 22:20:04 +0800

    add comment

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,20 @@
+# 重新拉取主题 
+# mkdir themes => cd themes => git clone https://github.com/liuzc/LeaveIt.git => hugo serve 即可
+
 # source ./deploy.sh 一键部署和备份博客
 # public不能删除（包含.git）
 # 生成public(替换)
 hugo
 # 备份博客内容
 git add .
-git commit -m "blog backup"
+git commit -m "add comment"
 git push origin
 # 推送静态文件到githubPage
 cd public
 # git init
 # git remote add origin git@github.com:mywebc/mywebc.github.io.git
 git add .
-git commit -m "blog update"
+git commit -m "add comment"
 git pull origin master
 git push origin master
 # 退出
